16e3d5353fae0a9427ca1225f6dfce4eaca4ab7b
Fede Cardoso <hernan.f.cardoso@gmail.com>
2025-11-18 16:44:52 -0300

    improve message

--- templates/notice-confirm.php
+++ templates/notice-confirm.php
@@ -1,5 +1,5 @@
 <?php if (!defined('ABSPATH')) exit; ?>
 
 <div class="confirm-email-notice">
-   I’ve just sent you a welcome email explaining what we do and how we work. If you have any questions, feel free to reach out — I read absolutely everything.
+   Welcome! I’ve just sent you a welcome email explaining what we do and how we work. If you have any questions, feel free to reach out — I read absolutely everything.
 </div>
